c52e91f5008d2b353bb0b93cb7d02d0bcae028c9
Matthew Garcia <gmatthew2765@gmail.com>
2025-11-15 12:16:48 -0800

    removed auth buttons

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -29,22 +29,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${roboto.className} antialiased`}>
-          <header className="fixed right-0 flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          {children}
-        </body>
+        <body className={`${roboto.className} antialiased`}>{children}</body>
         <Toaster />
       </html>
     </ClerkProvider>
