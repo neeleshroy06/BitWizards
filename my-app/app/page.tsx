26d55f10621ea111337e8c783996c16d2682a3ef
Matthew Garcia <gmatthew2765@gmail.com>
2025-11-15 11:06:35 -0800

    added background image to landing page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Press_Start_2P } from "next/font/google";
 import Link from "next/link";
-import SpriteDisplay from "@/components/SpriteDisplay";
 
 const pixelFont = Press_Start_2P({
   subsets: ["latin"],
@@ -10,13 +9,13 @@ const pixelFont = Press_Start_2P({
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-screen w-screen flex items-center justify-center bg-[url('/bg.png')] bg-cover">
       <div className="text-center space-y-4">
-        <h1 className={`font-bold text-3xl ${pixelFont.className}`}>
+        <h1 className={`font-bold text-3xl ${pixelFont.className} text-white`}>
           BitsWizard
         </h1>
         <Link href="/home">
-          <Button>Join The Adventure</Button>
+          <Button variant="secondary">Join The Adventure</Button>
         </Link>
       </div>
     </div>
