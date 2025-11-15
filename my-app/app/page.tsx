2930cedc25d79b5a70c8068070532bb2aac044c9
Matthew Garcia <gmatthew2765@gmail.com>
2025-11-15 09:38:07 -0800

    created simple landing page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Press_Start_2P } from "next/font/google";
+
+const pixelFont = Press_Start_2P({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function Home() {
   return (
-    <div>
-      <p>Fresh blank template!</p>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="text-center space-y-4">
+        <h1 className={`font-bold text-3xl ${pixelFont.className}`}>
+          BitsWizard
+        </h1>
+        <Button>Join The Adventure</Button>
+      </div>
     </div>
   );
 }
