5e8ce6014ea693a0f6c5cd929c61fd15ce0c1bef
Matthew Garcia <gmatthew2765@gmail.com>
2025-11-15 14:37:29 -0800

    added logo

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Press_Start_2P } from "next/font/google";
 import Link from "next/link";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
 
 const pixelFont = Press_Start_2P({
   subsets: ["latin"],
@@ -9,8 +11,13 @@ const pixelFont = Press_Start_2P({
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-[url('/bg.png')] bg-cover">
-      <div className="text-center space-y-4">
+    <div className="h-screen w-screen flex items-center justify-center bg-[url('/bg.png')] relative bg-cover">
+      <div className="text-center space-y-4 flex items-center flex-col">
+        <Image
+          src={Logo}
+          alt="BitsWizard Logo"
+          className="w-70 m-0 h-70 absolute top-25"
+        />
         <h1 className={`font-bold text-3xl ${pixelFont.className} text-white`}>
           BitsWizard
         </h1>
