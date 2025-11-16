d06fb81a32b4d58f4faa85c450dcb91469ca8ab1
Matthew Garcia <gmatthew2765@gmail.com>
2025-11-15 17:52:29 -0800

    added text to home page

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -6,8 +6,16 @@ import Workspace from "@/components/Workspace";
 const HomePage = () => {
   return (
     <div className="flex h-screen w-screen overflow-hidden">
-      <SidebarGlobal />
-      <Workspace />
+      <div>
+        <SidebarGlobal />
+      </div>
+      <div className="flex-1  flex flex-col">
+        <Workspace />
+        <div className="p-4 border-t">
+          <h1 className="font-bold">Playground Mode</h1>
+          <p>Welcome to the playground! Mix and match different blocks.</p>
+        </div>
+      </div>
     </div>
   );
 };
